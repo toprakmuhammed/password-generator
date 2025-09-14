ea01790007b6401df7841c91946f3aad816f0253
Muhammed Toprak <muhammedtoprak@live.com>
2025-09-14 17:44:35 +0300

    setup dom references, char sets and basic password generate options.

---
+++ script.js
@@ -0,0 +1,33 @@
+// DOM Elements - all the elements we need from HTML
+const passwordInput = document.getElementById("password");
+const lengthSlider = document.getElementById("length");
+const lengthDisplay = document.getElementById("length-value");
+const uppercaseCheckbox = document.getElementById("uppercase");
+const lowercaseCheckbox = document.getElementById("lowercase");
+const numbersCheckbox = document.getElementById("numbers");
+const symbolsCheckbox = document.getElementById("symbols");
+const generateButton = document.getElementById("generate-btn");
+const copyButton = document.getElementById("copy-btn");
+const strengthBar = document.getElementById(".strength-bar");
+const strengthText = document.getElementById(".strength-container p");
+const strengthLabel = document.getElementById("strength-label");
+
+// Character sets
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const numberCharacters = "0123456789";
+const symbolCharacters = "!@#$%^&*()-_=+[]{}|;:,.<>?/";
+
+lengthSlider.addEventListener("input", () => {
+  lengthDisplay.textContent = lengthSlider.value;
+});
+
+generateButton.addEventListener("click", makePassword);
+
+function makePassword() {
+  const length = Number(lengthSlider.value); //Normalde burası string (yazı) olarak gelir. Number ile sayıya çeviriyoruz. Artık length değişkeninde lengthSlider değerinin şifre uzunluğu var.
+  const includeUppercase = uppercaseCheckbox.checked;
+  const includeLowercase = lowercaseCheckbox.checked;
+  const includeNumbers = numbersCheckbox.checked;
+  const includeSymbols = symbolsCheckbox.checked;
+}
